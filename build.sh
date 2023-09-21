ffaf3fd7bdd6f015d3f41506784d3e0bde985fe0
2zqa <25235249+2zqa@users.noreply.github.com>
2023-09-22 01:03:09 +0200

    Add lint function

--- build.sh
+++ build.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 ignore="Makefile README.md manifest_*.json build.sh"
 
+function lint {
+    echo "=> Linting Firefox extension"
+    mv manifest_firefox.json manifest.json
+    web-ext lint --self-hosted --ignore-files=$ignore
+    firefox_exit_code=$?
+    mv manifest.json manifest_firefox.json
+    echo "=> Linting Chromium extension"
+    mv manifest_chromium.json manifest.json
+    web-ext lint --self-hosted --ignore-files=$ignore
+    chromium_exit_code=$?
+    mv manifest.json manifest_chromium.json
+    if [ $firefox_exit_code -eq 0 ] && [ $chromium_exit_code -eq 0 ]; then
+        echo "=> Successfully linted Firefox and Chromium extensions"
+    else
+        if [ $firefox_exit_code -ne 0 ]; then
+            echo "=> Errors occurred while linting Firefox extension"
+        fi
+        if [ $chromium_exit_code -ne 0 ]; then
+            echo "=> Errors occurred while linting Chromium extension"
+        fi
+    fi
+}
+
 function firefox {
     echo "=> Preparing to build Firefox extension"
     mv manifest_firefox.json manifest.json
@@ -29,6 +52,9 @@ function all {
 }
 
 case "$1" in
+    lint)
+        lint
+        ;;
     firefox)
         firefox
         ;;
@@ -42,7 +68,7 @@ case "$1" in
         rm -rf web-ext-artifacts/
         ;;
     *)
-        echo "Usage: $0 {firefox|chromium|all|clean}"
+        echo "Usage: $0 {firefox|chromium|all|clean|lint}"
         exit 1
 esac
 
